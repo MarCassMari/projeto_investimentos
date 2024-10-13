b620408817eb47f0b14b3cb9a2040da5a42ad921
Mariana Machado <marcassmachado32@gmail.com>
2024-10-12 23:51:54 -0300

    rotas e enviando dados para a tela

--- invista_me/views.py
+++ invista_me/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def pagina_inicial(request):
+    return HttpResponse('Página padrão pronta para ser preenchida')
+
+def contato_duvidas(request):
+    return HttpResponse('Em caso de dúvidas, envie uma mensagem para o email teste@gmail.com.org')
+
+def minha_historia(request):
+
+    pessoa = {'nome': 'Lucius','idade':20, 'hobby': 'Games'}
+    return render(request,'investimentos/minha_historia.html', pessoa)
--- projeto_invista/urls.py
+++ projeto_invista/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from invista_me import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   path('', views.pagina_inicial),
+   path('contato/', views.contato_duvidas, name='contato'),
+   path('minha_historia/', views.minha_historia,name ='minha_historia') 
 ]
