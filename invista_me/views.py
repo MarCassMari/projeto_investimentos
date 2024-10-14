dacc9fdbc0e0a48b8e41633a981ff0a7bb9ecffe
Mariana Machado <marcassmachado32@gmail.com>
2024-10-14 16:23:38 -0300

    Listagem na tela principal

--- invista_me/views.py
+++ invista_me/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import Investimento
 
 # Create your views here.
-
-def pagina_inicial(request):
-    return HttpResponse('Página padrão pronta para ser preenchida')
 
 def novo_investimento(request):
      return render(request,'investimentos/novo_investimento.html')
 
-def investimento_registrado(request):
-     investimento = {
-          'tipo_investimento': request.POST.get('TipoInvestimento') 
+def investimentos(request):
+     dados= {
+          'dados':  Investimento.objects.all()
      }
-     return render(request,'investimentos/investimento_registrado.html', investimento)
-def investimento(request):
-     return render(request,'')
+     
+     return render(request,'investimentos/investimentos.html', context=dados)
