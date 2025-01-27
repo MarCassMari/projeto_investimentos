f18a18ef3b285e713840e18ef8b475dd1d6de9d4
Mariana Machado <marcassmachado32@gmail.com>
2025-01-27 19:07:40 -0300

    Novos campos e deixando o design mais amigável

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
  
 # Create your views here.
 def novo_usuario(request):
     if request.method == 'POST':
-        formulario = UserCreationForm(request.POST)
+        formulario = UserRegisterForm(request.POST)
         if formulario.is_valid():
             formulario.save()
             usuario = formulario.cleaned_data.get('username')
@@ -13,6 +13,6 @@ def novo_usuario(request):
             return redirect('investimentos')
         
     else:
-        formulario = UserCreationForm()
+        formulario = UserRegisterForm()
        
     return render(request, 'usuarios/registrar.html', {'formulario': formulario})
