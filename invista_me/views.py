8ca228b86b54e7f2fd3ce5aa0da270f03915059e
Mariana Machado <marcassmachado32@gmail.com>
2025-01-28 13:02:28 -0300

    restringindo os acessos

--- invista_me/views.py
+++ invista_me/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import Investimento
 from .forms import InvestimentoForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -19,7 +20,7 @@ def detalhe(request, id_investimento):
    'dados':  Investimento.objects.get(pk=id_investimento)
      }
      return render(request, 'investimentos/detalhe.html',dados)
-
+@login_required
 def criar(request):
      if request.method == 'POST':
          investimento_form = InvestimentoForm(request.POST)
@@ -32,7 +33,7 @@ def criar(request):
           'formulario': investimento_form
      }
      return render(request,'investimentos/novo_investimento.html', context=formulario)
-
+@login_required
 def editar(request, id_investimento):
    investimento = Investimento.objects.get(pk=id_investimento)
    if request.method == 'GET':
@@ -43,7 +44,7 @@ def editar(request, id_investimento):
        if formulario.is_valid():
             formulario.save()
        return redirect('investimentos')     
-  
+@login_required  
 def excluir(request, id_investimento):
     investimento = Investimento.objects.get(pk=id_investimento)
     if request.method == 'POST':
